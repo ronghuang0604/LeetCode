467a3c88bb1d06b038df31caa95302c88622e1e3
Rong Huang <ronghuang0604@berkeley.edu>
2017-01-15 16:31:05 -0800

    sum without operators

--- sum_using_xor.py
+++ sum_using_xor.py
@@ -1,4 +1,8 @@
 class Solution(object):
+    # xor finds sum without carry
+    # and finds all the CARRY, but you need to shift it to the left for one place just like what you do in math
+    # then add the carry to the xor result, but since this is addition, you can just use recursion and set a = carry_without_carry, b = carry 
+
     def sum_using_xor(self, a, b):
         """
         :type a: int
